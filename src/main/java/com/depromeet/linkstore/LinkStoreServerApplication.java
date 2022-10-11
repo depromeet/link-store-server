a7724061d257ece16360b9ed7853e188e292b7b7
wooyounggggg <souljit2@gmail.com>
2022-10-11 22:14:06 +0900

    init: initialize

---
+++ src/main/java/com/depromeet/linkstore/LinkStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.linkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkStoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkStoreServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depromeet/linkstore/LinkStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.linkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkStoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
